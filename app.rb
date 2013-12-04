1087f0a36dfd86e8cf60b916c566726c6c49ccdb
Javier Mena <alu0100454741@ull.edu.es>
2013-12-04 10:15:16 +0000

    modificando app para que funcione con ajax

--- app.rb
+++ app.rb
@@ -142,9 +142,18 @@ module TicTacToe
   def computer_wins?
     winner == COMPUTER
   end
+
+  def tie?
+      ((winner != COMPUTER) && (winner != HUMAN)) 
+  end
 end
 
 helpers TicTacToe
+
+get "/" do 
+  session["bs"] = inicializa()
+  haml :game, :locals => { :b => board, :m => ''  }
+end
 
 get %r{^/([abc][123])?$} do |human|
   if human then
@@ -153,21 +162,22 @@ get %r{^/([abc][123])?$} do |human|
     pp session
     if legal_moves.include? human
       board[human] = TicTacToe::CIRCLE
-      # computer = board.legal_moves.sample
       computer = smart_move
-      redirect to ('/humanwins') if human_wins?
-      redirect to('/') unless computer
+      return '/humanwins' if human_wins?
+      return '/tie' unless computer
       board[computer] = TicTacToe::CROSS
       puts "I played: #{computer}!"
       puts "Tablero:  #{board.inspect}"
-      redirect to ('/computerwins') if computer_wins?
+      return '/computerwins' if computer_wins?
+      result = computer
     end
   else
     session["bs"] = inicializa()
     puts "session = "
     p session
+    result = "illegal"
   end
-  haml :game, :locals => { :b => board, :m => ''  }
+  result
 end
 
 get '/humanwins' do
@@ -184,7 +194,7 @@ get '/humanwins' do
             pp un_usuario
             p "---------"
           end
-          'Human wins'
+          '¡Tú ganas!'
         else 
           redirect '/'
         end
@@ -207,7 +217,29 @@ get '/computerwins' do
             un_usuario.partidas_perdidas = contador
             un_usuario.save
           end
-          'Computer wins'
+          '¡Gana la máquina!'
+        else 
+          redirect '/'
+        end
+    haml :final, :locals => { :b => board, :m => m }
+  rescue
+    redirect '/'
+  end
+end
+
+get '/tie' do
+  puts "/tie"
+  pp session
+  begin
+    m = if tie? then
+          if (session["usuario"] != nil)
+            un_usuario = Usuario.first(:username => session["usuario"])
+            contador = un_usuario.partidas_empatadas
+            contador = contador + 1
+            un_usuario.partidas_empatadas = contador
+            un_usuario.save
+          end
+          '¡Empate!'
         else 
           redirect '/'
         end
@@ -226,7 +258,7 @@ post '/' do
     session.clear
   else
     nick = params[:usuario]
-    #nick = nick["username"]
+    nick = nick["username"]
     u = Usuario.first(:username => "#{nick}" )
     if u == nil
       usuario = Usuario.create(params[:usuario])
